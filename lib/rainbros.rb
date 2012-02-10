f257ea96daed50ce87f6d36af269170b0601fca6
ian asaff <ian.asaff@gmail.com>
2012-02-10 17:03:53 -0500

    rainbros forever, better interrupt handling (actually exit)

--- lib/rainbros.rb
+++ lib/rainbros.rb
@@ -3,12 +3,13 @@ require "rainbros/version"
 module Rainbros
   module_function
   def bro_it_out!
+    cmd = 'dicks | lolcat'
     if ARGV[0] && ARGV[0] == "forever"
       while true
-        system('dicks | lolcat')
+        exit! unless system(cmd)
       end
     else
-      4.times { system('dicks | lolcat') }
+      4.times { system(cmd) }
     end
   end
 end
