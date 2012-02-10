967221698a9b0b89f679190ed6a7c3a3d49104f8
Andy LeClair <andy.leclair@gmail.com>
2012-02-10 12:20:23 -0500

    dude bros it goes on FOREVER

--- lib/rainbros.rb
+++ lib/rainbros.rb
@@ -3,6 +3,12 @@ require "rainbros/version"
 module Rainbros
   module_function
   def bro_it_out!
-   4.times { system('dicks | lolcat') }
+    if ARGV[0] && ARGV[0] == "forever"
+      while true
+        system('dicks | lolcat')
+      end
+    else
+      4.times { system('dicks | lolcat') }
+    end
   end
 end
